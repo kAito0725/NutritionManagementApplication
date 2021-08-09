8618f4c4e5c439e70df3bb075b199aadd884fb36
kAito <kaitoaoki0725@gmail.com>
2021-08-09 22:54:19 +0900

    Add Footer component.

---
+++ pages/components/footer/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="absolute bottom-1 ml-4 space-x-5">
+      <small>利用規約</small>
+      <small>Copyright © 2021 KAITO AOKI All Rights Reserved</small>
+    </footer>
+  )
+}
